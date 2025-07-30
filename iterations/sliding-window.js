3b34fa73c523a2b1afff9130ed43729d1cad5a8f
inazrabuu <inazrabuu@gmail.com>
2025-07-30 16:44:33 +0700

    change cache data type

--- iterations/sliding-window.js
+++ iterations/sliding-window.js
@@ -1,11 +1,13 @@
 function maxUniqueSubstrLength(s) {
   let left = 0, maxLen = 0
-  const cache = new Set()
+  const cache = new Map()
+
   for (let right = 0; right < s.length; right++) {
-    while (cache.has(s[right])) {
-      cache.delete(s[left++])
+    if (cache.has(s[right] && cache.get(s[right] >= left))) {
+      left = cache.get(s[right]) + 1
     }
-    cache.add(s[right])
+
+    cache.set(s[right], right)
 
     maxLen = Math.max(maxLen, right - left + 1)
   }
