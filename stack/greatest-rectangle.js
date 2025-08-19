f92272235e300d9e345584a891d1ce2284c2623e
inazrabuu <inazrabuu@gmail.com>
2025-08-19 10:16:35 +0700

    largest rectangle

---
+++ stack/greatest-rectangle.js
@@ -0,0 +1,23 @@
+const largestRectangle = function(heights) {
+  let size = 0
+  const stack = []
+
+  for (let i = 0; i <= heights.length; i++) {
+    let pole = i === heights.length ? 0 : heights[i]
+
+    console.log(stack)
+
+    while (stack.length && pole < heights[stack[stack.length - 1]]) {
+      let height = heights[stack.pop()],
+          width = stack.length ? i - stack[stack.length - 1] - 1 : i
+      size = Math.max(size, height * width)
+
+    }
+    stack.push(i)
+  }
+
+  return size
+}
+
+console.log(largestRectangle([3, 4, 2, 5, 7 ,1]))
+console.log(largestRectangle([1, 2, 3, 4, 5]))
